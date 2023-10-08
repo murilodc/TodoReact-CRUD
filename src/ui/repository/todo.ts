103d58e78ced6c4817cbb08bd346592b35af5301
muriloodecarli@gmail.com <murilo@Murilo>
2023-10-08 20:52:39 -0300

    Feito ajustes na tipagem do codigo

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -14,13 +14,17 @@ function get({
 }: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
   return fetch("api/todos").then(async (respostaDoServidor) => {
     const todosString = await respostaDoServidor.text();
-    const todosFromServer = JSON.parse(todosString).todos;
+    const todosFromServer = parseTodosFromServer(JSON.parse(todosString)).todos;
     const ALL_TODOS = todosFromServer;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
     const paginatedTodos = ALL_TODOS.slice(startIndex, endIndex);
     const totalPages = Math.ceil(ALL_TODOS.length / limit);
-    return { todos: paginatedTodos, total: ALL_TODOS.length, pages: 1 };
+    return {
+      todos: paginatedTodos,
+      total: ALL_TODOS.length,
+      pages: totalPages,
+    };
   });
 }
 
@@ -33,4 +37,39 @@ interface Todo {
   content: string;
   date: Date;
   done: boolean;
+}
+
+function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+  if (
+    responseBody !== null &&
+    typeof responseBody === "object" &&
+    "todos" in responseBody &&
+    Array.isArray(responseBody.todos)
+  ) {
+    return {
+      todos: responseBody.todos.map((todo: unknown) => {
+        if (todo == null && typeof todo !== "object") {
+          throw new Error("Invalid todo from API");
+        }
+
+        const { id, content, date, done } = todo as {
+          id: string;
+          content: string;
+          date: string;
+          done: string;
+        };
+
+        return {
+          id,
+          content,
+          date: new Date(date),
+          done: String(done).toLowerCase() === "true",
+        };
+      }),
+    };
+  }
+
+  return {
+    todos: [],
+  };
 }

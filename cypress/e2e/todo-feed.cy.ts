c0fdf6516970711771fc8fbb82cfb56904c5c985
Murilo De Carli <muriloodecarli@gmail.com>
2024-01-09 21:46:25 -0300

    boas praticas

--- cypress/e2e/todo-feed.cy.ts
+++ cypress/e2e/todo-feed.cy.ts
@@ -5,7 +5,7 @@ describe("/ - Todos Feed", () => {
     // Trailing Slash
     cy.visit(BASE_URL);
   });
-  it.only("when create a new todo, it must appears in the screen", () => {
+  it("when create a new todo, it must appears in the screen", () => {
     // 0 - Interceptações/Intertecptação
     cy.intercept("POST", `${BASE_URL}/api/todos`, (request) => {
       request.reply({
@@ -24,12 +24,10 @@ describe("/ - Todos Feed", () => {
     // 1 - Abrir a página
     cy.visit(BASE_URL);
     // 2 - Selecionar o input de criar nova todo
-    const $inputAddTodo = cy.get("input[name='add-todo']");
-    // 3 - Digitar no input de criar nova todo
-    $inputAddTodo.type("Test todo");
+    // 3 - Digitar no input de criar nova todoc
+    cy.get("input[name='add-todo']").type("Test todo");
     // 4 - Clicar no botão
-    const $btnAddTodo = cy.get("[aria-label='Adicionar novo item']");
-    $btnAddTodo.click();
+    cy.get("[aria-label='Adicionar novo item']").click();
 
     // 5 - Checar se na página surgiu um novo elemento
     cy.get("table > tbody").contains("Test todo");
